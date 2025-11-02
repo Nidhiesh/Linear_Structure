ba9aeac30906de5a7d674ced6c2093dc3d3cbde8
Nidhiesh <kit28.24cs104@gmail.com>
2025-11-02 16:09:35 +0530

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class DoublyLinkedList {
     class Node {
         int data;
@@ -27,7 +29,7 @@ public class DoublyLinkedList {
     }
 
     // Code to insert Node at particular position
-    public void insert_at_position(int data, int pos) {
+    public void insert_at_particular_position(int data, int pos) {
         Node nn = new Node(data);
         Node temp = head;
         if (pos == 0) {
@@ -82,7 +84,7 @@ public class DoublyLinkedList {
     }
 
     // Code to delete Node at particular position
-    public void delete_at_position(int pos) {
+    public void delete_at_particular_position(int pos) {
         Node temp = head;
         if (pos == 0) {
             if (head == null) {
@@ -135,22 +137,76 @@ public class DoublyLinkedList {
 
     // Main block of code
     public static void main(String[] args) {
-        DoublyLinkedList n = new DoublyLinkedList();
-        n.insert_at_begin(5);
-        n.display();
-        n.insert_at_begin(10);
-        n.display();
-        n.insert_at_end(12);
-        n.display();
-        n.insert_at_end(20);
-        n.display();
-        n.delete_at_begin();
-        n.display();
-        n.delete_at_end();
-        n.display();
-        n.insert_at_position(3, 2);
-        n.display();
-        n.delete_at_position(0);
-        n.display();
+    	DoublyLinkedList n = new DoublyLinkedList();
+    	try(Scanner c = new Scanner(System.in)){
+    		
+//	        Scanner c = new Scanner(System.in);
+	        boolean flag = true;
+	        int a,b;
+	        while(flag) {
+	        	System.out.println("********************************************************************************");
+	        	System.out.println("1.Insert at begin\n2.Insert at particular position\n3.Insert at end\n4.Delete at begin\n5.Delete at particular position\n6.Delete at end\n7.Display all nodes\n7.Exit");
+	        	System.out.print("Enter your choice : ");
+	        	int choice = c.nextInt();
+	        	switch(choice) {
+	        	case 1:
+	        		
+	        		System.out.print("Enter value to insert : ");
+	        		a = c.nextInt();
+	        		n.insert_at_begin(a);
+	        		System.out.println(a+" is inserted at the begin");
+	        		break;
+	        	
+	        	case 2:
+	        		
+	        		System.out.print("Enter value to insert : ");
+	        		a = c.nextInt();
+	        		System.out.print("Enter the position to insert : ");
+	        		b = c.nextInt();
+	        		n.insert_at_particular_position(a,b);
+	        		System.out.println(a+" is inserted at the position "+b+"/n /n");
+	        		break;
+	        		
+	        	case 3:
+	        		
+	        		System.out.print("Enter value to insert : ");
+	        		a = c.nextInt();
+	        		n.insert_at_end(a);
+	        		System.out.println(a+" is inserted at the the end/n/n");
+	        		break;
+	        		
+	        	case 4:
+	        		n.delete_at_begin();
+	        		System.out.println("First node is deleted/n/n");
+	        		break;
+	        		
+	        	case 5:
+	        		System.out.print("Enter the position to delete : ");
+	        		a = c.nextInt();
+	        		n.delete_at_particular_position(a);
+	        		System.out.println("Node is deleted at the position "+a+"\n\n");
+	        		break;
+	        		
+	        	case 6:
+	        		n.delete_at_end();
+	        		System.out.println("Last node is deleted\n\n");
+	        		break;
+	        		
+	        	case 7:
+	        		n.display();
+	        		System.out.println("All datas are displayed\n\n");
+	        		break;
+	        		
+	        	case 8:
+	        		System.out.println("Program is going to exit\n\n");
+	        		flag = false;
+	        		break;
+	        		
+	        	default:
+	        		System.out.println("Invalid choice\n\n");
+	        		break;
+	        	}
+	        }
+	    }
     }
 }
